e36cfc7b2858dd3fb61594fe6bab857d8cef54d5
Benjamin-Joe <benjaminjoe43@gmail.com>
2022-04-16 11:55:34 +0000

    Add search abiliy for search bar in base.html

--- products/views.py
+++ products/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
 def all_products(request):
     "A view to display all of the products"
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, 'Woopsie, Not A Valid Input')
+                return redirect(reverse, 'products')
+            queries = Q(title__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
